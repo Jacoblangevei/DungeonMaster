045c50992359cef90362f981e9700179b40f4cd2
Jacob Langevei <jacob-langevei@hotmail.com>
2023-09-11 09:34:38 +0200

    Add project files.

---
+++ DungeonMaster/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance of the DungeonMaster class
+        DungeonMaster dm = new DungeonMaster();
+
+    }
+}
